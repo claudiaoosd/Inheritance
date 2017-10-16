a73ecda5a54bb74898bf4a0a238a5552b182a48d
javaclass <javaclass@15000515cpu.fec.org>
2017-10-16 16:16:18 -0500

    0

---
+++ src/inheritance/SalesPerson.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class SalesPerson extends Employee {
+
+	void fourOoneK() {
+		System.out.println("process salesperson 401k");
+	}
+
+	public void bonusEmp() {
+		System.out.println("The bonus for salesperson is 3.5");
+
+	}
+
+
+}
